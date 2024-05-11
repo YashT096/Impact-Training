4c72054c6d429839b3bfab98dd548094d812ea86
YashT096 <109242952+YashT096@users.noreply.github.com>
2024-05-11 10:38:28 +0530

    update

---
+++ Day1-2/Day3.js
@@ -0,0 +1,4 @@
+y="Javascript ";
+y.trim();
+console.log(y.trim()); 
+console.log(y.slice(2,4));
---
+++ Day1-2/index.js
@@ -0,0 +1,34 @@
+let day=6;
+
+if(day==0)
+{
+    console.log("Invalid Input");
+}
+else if(day==1)
+    {
+        console.log("Today is Monday");
+    }
+    else if(day==2)
+        {
+            console.log("Today is Tuesday");
+        } else if(day==3)
+            {
+                console.log("Today is Wednesday");
+            } else if(day==4)
+                {
+                    console.log("Today is Thursday");
+                }else if(day==5)
+                    {
+                        console.log("Today is Friday");
+                    } else if(day==6)
+                        {
+                            console.log("Today is Saturday");
+                        }else if(day==7)
+                            {
+                                console.log("Today is Sunday");
+                            }
+else
+{
+    console.log("Invalid Input");
+}
+

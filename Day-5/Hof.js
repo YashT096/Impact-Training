6d2ecfb8f9b3adaa4591338606f50e1a3d924a55
YashT096 <109242952+YashT096@users.noreply.github.com>
2024-05-14 10:57:45 +0530

    DAY-5update

---
+++ Day-5/Hof.js
@@ -0,0 +1,15 @@
+
+function check(marksheet, name) {
+    console.log("Hello $(name)");
+    marksheet();
+}check ("marksheet, abx);
+"
+function marksheet (){
+    let marks = math.floor(math.random(*10)+ 1 );
+    if(marks>5){
+        console.log("Congratulations! You have passed");
+
+    }
+}else {
+    console.log ("Better luck next timee")
+}
